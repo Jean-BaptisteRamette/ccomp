451b13fb38dd85d65dfb177c3f39fd5758d52a08
Jean-BaptisteRamette <jeanbaptisterametteperso@gmail.com>
2023-03-12 00:59:27 +0100

    Simple stream-like interface for easier lexing, and avoid copies with string_view

---
+++ include/ccomp/stream.hpp
@@ -0,0 +1,31 @@
+#ifndef CCOMP_STREAM_INTERFACE_HPP
+#define CCOMP_STREAM_INTERFACE_HPP
+
+
+#include <fstream>
+
+
+namespace ccomp
+{
+	class stream
+	{
+	public:
+		explicit stream(std::ifstream& src_stream);
+		explicit stream(std::string&& src_stream);
+
+		bool eof();
+		char get();
+		char peek();
+		void seek(size_t offset);
+		size_t tellg();
+		const char* iterator();
+
+	private:
+		std::string buffer;
+
+		size_t cursor {};
+	};
+}
+
+
+#endif //CCOMP_STREAM_INTERFACE_HPP
---
+++ src/stream.cpp
@@ -0,0 +1,51 @@
+#include <ccomp/stream.hpp>
+
+
+namespace ccomp
+{
+	stream::stream(std::ifstream& src_stream) : buffer(std::istreambuf_iterator<char>(src_stream), std::istreambuf_iterator<char>()), cursor { 0 } {}
+
+	stream::stream(std::string&& src_stream) : buffer(std::move(src_stream)), cursor { 0 } {}
+
+	bool stream::eof()
+	{
+		return cursor >= buffer.size();
+	}
+
+	char stream::get()
+	{
+		if (eof())
+			return 0;
+
+		return buffer[cursor++];
+	}
+
+	char stream::peek()
+	{
+		if (eof())
+			return 0;
+
+		return buffer[cursor];
+	}
+
+	void stream::seek(size_t offset)
+	{
+		if (offset >= buffer.size())
+			throw std::runtime_error("stream.cpp: stream::seek offset >= buffer.size()\n");
+
+		cursor = offset;
+	}
+
+	size_t stream::tellg()
+	{
+		return cursor;
+	}
+
+	const char* stream::iterator()
+	{
+		if (eof())
+			return nullptr;
+
+		return &buffer[cursor];
+	}
+}
